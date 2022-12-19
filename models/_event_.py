466768f65f7401786af0c17d80d9e1f0b8e00d93
JolyNote <73586773+JolyNote@users.noreply.github.com>
2022-12-19 17:52:44 +0500

    Update _event_.py

--- models/_event_.py
+++ models/_event_.py
@@ -9,5 +9,5 @@ Base = declarative_base()
 class Event(Base):
     __tablename__ = "event"
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey("telegram_user.id"), unique=True)
-    santa_id = Column(Integer, ForeignKey("telegram_user.id"), unique=True)
+    user_id = Column(Integer, ForeignKey("telegram_user.id"), unique=True) //кому дарят
+    santa_id = Column(Integer, ForeignKey("telegram_user.id"), unique=True) //кто дарит
